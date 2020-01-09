971913cd769f7e4fb5b69a1d5873acfe2a98afab
Jessica Wolff <jleahwolff@gmail.com>
2020-01-09 10:25:40 -0600

    Working on the home page.

--- front-end/src/components/homeRestaurants.js
+++ front-end/src/components/homeRestaurants.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import home from '../components/homecss.css';
+
+
 
 const homeRestaurants = () => {
     return(
         <div>
-            HOME PAGE
+            <h2 className="header">Home Page</h2>
         
             <button className="button"><Link to={'/addrestaurant'}>Add Restaurant</Link></button>
 
